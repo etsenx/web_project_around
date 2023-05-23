aaa24855344997b20fcd4943ef8f8ae863522eb6
Steven <stipen08@gmail.com>
2023-05-23 14:47:46 +0700

    feature: Added disabling save button function when imput is empty

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,28 @@ const closeButton = document.querySelector(".popup__close-button");
 const saveButton = document.querySelector(".popup__save");
 const name = document.querySelector(".profile-info__name");
 const about = document.querySelector(".profile-info__about");
+const nameInput = document.querySelector(".popup__input-name");
+const aboutInput = document.querySelector(".popup__input-about");
+
+function enableButton() {
+  if (nameInput.value === "" || aboutInput.value === "") {
+    saveButton.disabled = true;
+    saveButton.classList.add("popup__save_disabled");
+  } else {
+    saveButton.disabled = false;
+    saveButton.classList.remove("popup__save_disabled");
+  }
+}
 
 editButton.addEventListener("click", () => {
-  let nameInput = document.querySelector(".popup__input-name");
-  let aboutInput = document.querySelector(".popup__input-about");
   nameInput.value = name.textContent;
   aboutInput.value = about.textContent;
-  popup.style.display = "block";
+  popup.classList.add("popup_opened");
 })
 
 closeButton.addEventListener("click", () => {
-  popup.style.display = "none";
+  popup.classList.remove("popup_opened");
+  saveButton.classList.remove("popup__save_disabled");
 })
 
 saveButton.addEventListener("click", () => {
@@ -23,5 +34,5 @@ saveButton.addEventListener("click", () => {
 
   name.textContent = saveName.value;
   about.textContent = saveAbout.value;
-  popup.style.display = "none";
+  popup.classList.remove("popup_opened");
 })
