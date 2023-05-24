40e25ddf420c80fc99a8bdcf3664bcfd8936db97
Steven <stipen08@gmail.com>
2023-05-24 09:16:58 +0700

    fix: Optimized Code

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,25 @@
-const popup = document.querySelector(".popup");
-const editButton = document.querySelector(".profile-info__edit-button");
-const closeButton = document.querySelector(".popup__close-button");
-const saveButton = document.querySelector(".popup__save");
-const name = document.querySelector(".profile-info__name");
-const about = document.querySelector(".profile-info__about");
-const nameInput = document.querySelector(".popup__input-name");
-const aboutInput = document.querySelector(".popup__input-about");
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile-info__edit-button');
+const closeButton = document.querySelector('.popup__close-button');
+const name = document.querySelector('.profile-info__name');
+const about = document.querySelector('.profile-info__about');
+const nameInput = document.querySelector('.popup__input-name');
+const aboutInput = document.querySelector('.popup__input-about');
 
-function enableButton() {
-  if (nameInput.value === "" || aboutInput.value === "") {
-    saveButton.disabled = true;
-    saveButton.classList.add("popup__save_disabled");
-  } else {
-    saveButton.disabled = false;
-    saveButton.classList.remove("popup__save_disabled");
-  }
-}
-
-editButton.addEventListener("click", () => {
+editButton.addEventListener('click', () => {
   nameInput.value = name.textContent;
   aboutInput.value = about.textContent;
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
 })
 
-closeButton.addEventListener("click", () => {
-  popup.classList.remove("popup_opened");
-  saveButton.disabled = false;
-  saveButton.classList.remove("popup__save_disabled");
+closeButton.addEventListener('click', () => {
+  popup.classList.remove('popup_opened');
 })
 
-saveButton.addEventListener("click", () => {
-  const saveName = document.querySelector(".popup__input-name");
-  const saveAbout = document.querySelector(".popup__input-about");
-
-  name.textContent = saveName.value;
-  about.textContent = saveAbout.value;
-  popup.classList.remove("popup_opened");
+//Mr Oksana Sergeeva, I tried using submit and it doesn't work. Give me a little bit hint
+//Im thinking that should I add form tag to the HTML will that work?
+document.querySelector('.popup__save').addEventListener('click', () => {
+  name.textContent = nameInput.value;
+  about.textContent = aboutInput.value;
+  popup.classList.remove('popup_opened');
 })
