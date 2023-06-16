b17ea5c89d56ea84e3051fac4302001ca213ac98
Steven <steven@pop-os.localdomain>
2023-06-16 10:09:57 +0700

    Added add card feature

--- scripts/script.js
+++ scripts/script.js
@@ -25,42 +25,93 @@ const initialCards = [
   },
 ];
 
-const popup = document.querySelector(".popup");
+const allCloseButton = document.querySelectorAll(".popup__close-button");
 const editButton = document.querySelector(".profile-info__edit-button");
-const closeButton = document.querySelector(".popup__close-button");
-const name = document.querySelector(".profile-info__name");
-const about = document.querySelector(".profile-info__about");
-const nameInput = document.querySelector(".popup__input-name");
-const aboutInput = document.querySelector(".popup__input-about");
-const popupForm = document.querySelector(".popup__form");
+const cardAddButton = document.querySelector(".profile__add-button");
 const elementSection = document.querySelector(".elements");
+const profileName = document.querySelector(".profile-info__name");
+const profileAbout = document.querySelector(".profile-info__about");
+
+// Edit Profile Popup
+const editProfilePopup = document.querySelector(".popup_edit");
+const editPopupNameInput = editProfilePopup.querySelector(".popup__input-name");
+const editPopupAboutInput = editProfilePopup.querySelector(
+  ".popup__input-about"
+);
+const editPopupForm = editProfilePopup.querySelector(".popup__form");
+
+// Add Card Popup
+const addCardPopup = document.querySelector(".popup_add");
+const addCardInputTitle = addCardPopup.querySelector(".popup__input-title");
+const addCardInputUrl = addCardPopup.querySelector(".popup__input-url");
+const saveButton = addCardPopup.querySelector(".popup__save");
+const addCardPopupForm = addCardPopup.querySelector(".popup__form");
 
 initialCards.forEach((card) => {
+  elementSection.append(createCard(card));
+});
+
+function createCard(card) {
   const elementTemplate = document.querySelector("#element-template").content;
   const newElement = elementTemplate.querySelector(".element").cloneNode(true);
   newElement.querySelector(".element__title").textContent = card.name;
   newElement.querySelector(".element__image").src = card.link;
   newElement.querySelector(".element__image").alt = card.name;
-  elementSection.append(newElement);
-});
-
-function edit() {
-  nameInput.value = name.textContent;
-  aboutInput.value = about.textContent;
-  popup.classList.add("popup_opened");
+  return newElement;
 }
 
-function close() {
-  popup.classList.remove("popup_opened");
+function editProfilePopupOpen() {
+  editPopupNameInput.value = profileName.textContent;
+  editPopupAboutInput.value = profileAbout.textContent;
+  editProfilePopup.classList.add("popup_opened");
+}
+
+function addCardPopupOpen() {
+  addCardPopup.classList.add("popup_opened");
+}
+
+function closePopup() {
+  editProfilePopup.classList.remove("popup_opened");
+  addCardPopup.classList.remove("popup_opened");
 }
 
 function save(event) {
   event.preventDefault();
-  name.textContent = nameInput.value;
-  about.textContent = aboutInput.value;
-  popup.classList.remove("popup_opened");
+  profileName.textContent = editPopupNameInput.value;
+  profileAbout.textContent = editPopupAboutInput.value;
+  editProfilePopup.classList.remove("popup_opened");
 }
 
-editButton.addEventListener("click", edit);
-closeButton.addEventListener("click", close);
-popupForm.addEventListener("submit", save);
+function isInputEmpty() {
+  const inputTitle = addCardInputTitle.value;
+  const inputUrl = addCardInputUrl.value;
+  // Profile form
+  if (inputTitle === "" || inputUrl === "") {
+    saveButton.classList.add("popup__save_disabled");
+  } else {
+    saveButton.classList.remove("popup__save_disabled");
+  }
+}
+
+function addCard(event) {
+  event.preventDefault();
+  const inputTitle = addCardInputTitle.value;
+  const inputUrl = addCardInputUrl.value;
+  initialCards.unshift({
+    name: inputTitle,
+    link: inputUrl
+  })
+  addCardPopup.classList.remove("popup_opened");
+  const newCard = createCard(initialCards[0])
+  elementSection.prepend(newCard)
+}
+
+editButton.addEventListener("click", editProfilePopupOpen);
+allCloseButton.forEach((button) => {
+  button.addEventListener("click", closePopup);
+});
+editPopupForm.addEventListener("submit", save);
+addCardInputTitle.addEventListener("keypress", isInputEmpty);
+addCardInputUrl.addEventListener("keypress", isInputEmpty);
+cardAddButton.addEventListener("click", addCardPopupOpen);
+addCardPopupForm.addEventListener("submit", addCard);
